5903244bfa28011b9a02424d4d78004c9cf1f7e7
Tri Mulyanto <trimulyanto.tm@gmail.com>
2019-11-30 18:05:53 +0700

    remove unused route

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Usage from '@/components/Usage'
-import Playground from '@/components/Playground'
-import Simple from '@/components/Simple'
-import VuelidateV from '@/components/VuelidateV'
 import Methods from '@/components/Methods'
 import Computed from '@/components/Computed'
 import Watchers from '@/components/Watchers'
@@ -16,26 +12,6 @@ const router = new Router({
         {
             path:'/',
             redirect:'/methods'
-        },
-        {
-            path:'/usage',
-            name:'Usage',
-            component: Usage
-        },
-        {
-            path:'/playground',
-            name:'Playground',
-            component: Playground
-        },
-        {
-            path:'/simple',
-            name:'Simple',
-            component: Simple
-        },
-        {
-            path:'/vuelidate',
-            name:'VuelidateV',
-            component: VuelidateV
         },
         {
             path:'/methods',
