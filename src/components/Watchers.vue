baea043e08770d0bcbb7466f9a6446e82f472d4e
Tri Mulyanto <trimulyanto.tm@gmail.com>
2019-12-16 09:20:43 +0700

    update dependencies serialize-javascript vulnerability

--- src/components/Watchers.vue
+++ src/components/Watchers.vue
@@ -1,32 +1,30 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col cols="12" md="6">
-                <v-sheet elevation="12" class="pa-12">
-                    <v-text-field label="Kilometer" v-model="kilometers"></v-text-field>
-                    <v-text-field label="Meters" v-model="meters"></v-text-field>
-                </v-sheet>
-            </v-col>
-           
-        </v-row>
-    </v-container>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-sheet elevation="12" class="pa-12">
+          <v-text-field label="Kilometer" v-model="kilometers"></v-text-field>
+          <v-text-field label="Meters" v-model="meters"></v-text-field>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
 export default {
-    data: () => ({
-        kilometers: 0,
-        meters:0
-        
-    }),
-    
-    watch: {
-        kilometers() {
-            this.meters = this.kilometers * 1000
-        },
-        meters(){
-            this.kilometers = this.meters/1000
-        }
+  data: () => ({
+    kilometers: 0,
+    meters: 0
+  }),
+
+  watch: {
+    kilometers() {
+      this.meters = this.kilometers * 1000;
+    },
+    meters() {
+      this.kilometers = this.meters / 1000;
     }
-}
+  }
+};
 </script>
 
